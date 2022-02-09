a759a4e1023347af0c0deed33c943753109b0e92
Anoy <anoy.eatl@gmail.com>
2022-02-09 23:49:28 +0600

    Updated

--- IWP/LoginNew.js
+++ IWP/LoginNew.js
@@ -1,22 +1,23 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
-
-// (async function start() {
-//   login();
-// })();
-
 const login = async () => {
   try {
     let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get("http://localhost:3002/");
+    await driver.get("http://localhost:3002");
     await driver
       .findElement(By.xpath("/html/body/div/main/div/main/div/a/button"))
       .click();
-    await driver.findElement(By.name("username")).sendKeys("admin", Key.RETURN);
-    await driver.findElement(By.name("password")).sendKeys("1", Key.RETURN);
+    if (
+      driver.findElement(By.name("username")).sendKeys("admin", Key.RETURN) &&
+      driver.findElement(By.name("password")).sendKeys("1", Key.RETURN)
+    ) {
+      console.log("Passed!!");
+    } else {
+      console.log("Failed");
+    }
   } catch (error) {
     console.log(error);
   } finally {
-    // await driver.quit();
+    await driver.quit();
   }
 };
 login();
